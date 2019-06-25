a282f6c8fc0fb3f6bdfa3987c4bcef315cde9732
sudhirpundir1 <sudhirpundir1@gmail.com>
2019-06-25 07:40:08 +0530

    todays session - tuple data type

---
+++ Tuple Data Type.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Tuple data type:-
+# in tuple we can not change the items once assigned... #immutable data type...!
+# ()
+
+
+# In[14]:
+
+
+dimensions=(200,500)
+print(dimensions)
+
+
+# In[15]:
+
+
+print(dimensions[0])
+print(dimensions[1])
+
+
+# In[19]:
+
+
+for i in range(len(dimensions)):
+    print(i)
+
+
+# In[18]:
+
+
+#reassigning the values to tuple
+
+dimensions=(500, 400)
+print(dimensions)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
