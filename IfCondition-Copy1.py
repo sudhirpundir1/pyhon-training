408fb85ddd243c36d4312eb0ccc1f3371a1912b1
sudhirpundir1 <sudhirpundir1@gmail.com>
2019-06-26 07:41:15 +0530

    Add files via upload

---
+++ IfCondition-Copy1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+#if Condition -- test the conditions defined
+cars=['audi','bmw','honda','toyota']
+
+#If cars= bmw then it should be in UPCASE letters, else other values turned to title case.
+
+for i in cars:
+    if i  == 'bmw':
+        print(i.upper())
+    else:
+        print(i.title())
+
+
+# In[2]:
+
+
+# = assigning a variable
+# == eqality testing
+
+
+# In[ ]:
+
+
+
+
