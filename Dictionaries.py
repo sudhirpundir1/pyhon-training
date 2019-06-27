4454c26ad00356c2d53b7e45d6dd175fd9dc60ef
sudhirpundir1 <sudhirpundir1@gmail.com>
2019-06-27 07:31:23 +0530

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Introduction to Dictionary:-
+# It is an mutable Data type: The value can be reassigned after declaration of variables.
+# Its a Key, Value pairs.
+# Declared in {Key:Value,Key:Value,.....}
+
+
+# In[8]:
+
+
+example_1 = {'color': 'green','points': 5,}
+
+
+# In[9]:
+
+
+print(example_1['color'])
+print(example_1['points'])
+
+
+# In[10]:
+
+
+example_2={'color':'red','points':10}
+print(example_2['color'])
+print(example_2['points'])
+
+
+# In[ ]:
+
+
+
+
