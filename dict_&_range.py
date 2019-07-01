be4a5ec893a8651b5dca60110283744f37b81441
sudhirpundir1 <sudhirpundir1@gmail.com>
2019-07-01 07:46:21 +0530

    Add files via upload

---
+++ dict_&_range.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(range(10))
+
+
+# In[2]:
+
+
+list(range(10))
+
+
+# In[3]:
+
+
+list(range(2,10,2))
+
+
+# In[7]:
+
+
+type(range(10))
+
+
+# In[8]:
+
+
+d={'sudhir':'vivo','monu':'samsung','vikash':'iphone'}
+d.keys()
+
+
+# In[10]:
+
+
+d.values()
+
+
+# In[11]:
+
+
+d.get('monu')
+
+
+# In[ ]:
+
+
+
+
