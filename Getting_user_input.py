ac603066239826c61626b761608fc87c5c60af6a
sudhirpundir1 <sudhirpundir1@gmail.com>
2019-07-01 07:42:29 +0530

    Add files via upload

---
+++ Getting_user_input.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+## INPUT func() always read values in string type
+x=input('print 1st value: ')
+a=int(x)
+y=input('print 1st value: ')
+b=int(y)
+z=a+b
+print(z)
+
+
+# In[3]:
+
+
+## INPUT func() always read values in string type
+x=int(input('print 1st value: '))
+
+y=int(input('print 1st value: '))
+
+print(f' The sum is: {x+y}')
+
+
+# In[4]:
+
+
+##Input a char, 
+ch=input('enter a char: ')
+print(ch)
+
+
+# In[ ]:
+
+
+##Input a char, if you want to print only single char the use index
+ch=input('enter a char: ')
+print(ch[0])
+
+
+# In[ ]:
+
+
+##Input a char, if you restrict input method to pick only 1st char
+ch=input('enter a char: ')[0]
+print(ch)
+
+
+# In[ ]:
+
+
+
+
---
+++ Getting_user_input_2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+##Input a char, 
+ch=input('enter a char: ')
+print(ch)
+
+
+# In[2]:
+
+
+##Input a char, if you want to print only single char then use index
+ch=input('enter a char: ')
+print(ch[0])
+
+
+# In[3]:
+
+
+##Input a char, if you restrict input method to pick only 1st char
+ch=input('enter a char: ')[0]
+print(ch)
+
+
+# In[5]:
+
+
+# if you want to pass a expression and want result. Pass: 2+7-4
+result=eval(input('enter a expression: ')) #pass 2+7-4
+print(result)
+
+
+# In[ ]:
+
+
+
+
